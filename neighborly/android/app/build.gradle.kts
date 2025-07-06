2e9b04bc3245bc6b8d5af658f64e1109d64e4bf6
Ruslan622 <sunbeebxy@gmail.com>
2025-07-06 15:47:22 +0600

    ndkVersion

--- neighborly/android/app/build.gradle.kts
+++ neighborly/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.neighborly"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "25.2.9519653"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
