55b1d9abf8bcb92cb0dabe03200d078f344545a1
Rakeentheboss <islamrakeen8@gmail.com>
2025-08-05 00:59:07 +0600

    Connected to backend blood donation

--- neighborly/lib/pages/blood_donation.dart
+++ neighborly/lib/pages/blood_donation.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+//connected to Firebase Firestore
 class BloodDonationPage extends StatefulWidget {
   const BloodDonationPage({super.key, this.initialTabIndex = 0});
 
