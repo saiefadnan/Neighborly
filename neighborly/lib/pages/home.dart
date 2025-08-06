60d1adcdc177a37d529a5fbbaa7fa7650173f386
saiefadnan <saiefadnan078@gmail.com>
2025-08-06 21:18:18 +0600

    authentication update

--- neighborly/lib/pages/home.dart
+++ neighborly/lib/pages/home.dart
@@ -6,7 +6,6 @@ import 'package:neighborly/pages/authPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:neighborly/app_routes.dart';
 import 'community_list.dart';
 import 'profile.dart';
