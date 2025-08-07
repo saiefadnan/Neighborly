7b7132e65fa7ae13d766aaf0f84d05bad83bfe12
sayef312469 <202214069@student.mist.ac.bd>
2025-08-08 01:31:12 +0600

    S

--- neighborly/lib/functions/token_verify.dart
+++ neighborly/lib/functions/token_verify.dart
@@ -3,11 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 Future<bool> verifyToken(String? idToken) async {
-<<<<<<< HEAD
-  final url = Uri.parse('http://192.168.1.183:4000/api/auth/signin/idtoken');
-=======
   final url = Uri.parse('${dotenv.env['BASE_URL']}/api/auth/signin/idtoken');
->>>>>>> cde855b5e12ce190dd7378473800ec411ce36514
   try {
     final response = await http.post(
       url,
