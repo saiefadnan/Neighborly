ee1c069d752b11dbe24e14e10dc141d00b6495c4
Ruslan622 <sunbeebxy@gmail.com>
2025-08-23 11:46:41 +0600

    routes for edit profile backend

---
+++ backend/routes/infosRoute.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono';
+import { getUserInfo, updateUserInfo } from '../controllers/editInfosController';
+
+const infosRouter = new Hono();
+
+// Get user info (view current values)
+infosRouter.get('/info', getUserInfo);
+
+// Update user info (insert/update allowed fields)
+//infosRouter.post('/info', updateUserInfo);
+infosRouter.get('/test', (c) => c.text('Test route works!'));
+
+export default infosRouter;
